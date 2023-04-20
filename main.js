d887ba4fbfc0a832162847f937902ad0f0b0a392
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-04-20 12:41:18 -0500

    [TAREA] sensei
    Add sensei class, ninja class child

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ class Ninja {
 		this.fuerza = 3;
 	}
 	sayName() {
-		console.log(`Este ninja es llamado ${this.nombre}-sensei`)
+		console.log(`Este ninja es llamado ${this.nombre}`)
 	}
 	showStats() {
 		console.log(`Ninja stats:\nNombre: ${this.nombre}\nFuerza: ${this.fuerza}\nVelocidad: ${this.velocidad}\nSalud: ${this.salud}`)
@@ -17,9 +17,29 @@ class Ninja {
 	}
 }
 
-const myNinja = new Ninja("Diego", 100)
+class Sensei extends Ninja {
+	constructor(nombre) {
+		super(nombre, 200);
+		this.velocidad = 10;
+		this.fuerza = 10;
+		this.sabiduria = 10;
+	}
+	speakWisdom() {
+		this.drinkSake();
+		console.log(`Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.`)
+	}
+	/* drinkSake() {
+		this.velocidad -= 3;
+	} */
+}
 
-myNinja.showStats()
-myNinja.drinkSake()
-myNinja.showStats()
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
+
+
 
